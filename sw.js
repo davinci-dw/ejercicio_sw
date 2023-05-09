d7f08f2cb4060b8da79e01675c307e53dfe36fcf
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-05-08 22:58:05 -0300

    feat: add files to cache

--- sw.js
+++ sw.js
@@ -4,11 +4,33 @@ self.addEventListener('install', event => {
         //aca instalo las cosas
         const cache = await caches.open('cache-1');
         await cache.addAll([
+            'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css',
             '/assets/fondo.jpg',
             '/assets/pwa.png',
+            '/assets/style.css',
+            'index.html',
         ]);
         await self.skipWaiting();
         resolve();
     });
     event.waitUntil(instalacion);
+});
+
+self.addEventListener('fetch', event => {
+    if(
+        event.request.url.includes('.html') ||
+        event.request.url.includes('fondo.jpg') ||
+        event.request.url.includes('bootstrap.min.css') ||
+        event.request.url.includes('style.css')){
+        //const respuesta = fetch('/assets/fondo.jpg'); //devuelvo descarga del sw
+        //event.respondWith(respuesta);
+        event.respondWith(
+            (async function() {
+                const cache = await caches.open('cache-1');
+                const respuesta = await cache.match(event.request);
+                if(respuesta) return respuesta;
+            })()
+        );
+
+    }
 });
